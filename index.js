a43c12af101a7da9a505ac3bb93c1f94f56c7acf
Macit IŞIK <macit.isik@gmail.com>
2025-01-13 23:23:21 +0300

    add time animnation

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function createWindow() {
     }
   })
 
-  win.removeMenu()
+  //win.removeMenu()
 
   win.show()
 
--- twofa.js
+++ twofa.js
@@ -5,7 +5,6 @@ const bootstrap = require('bootstrap')
 const Sortable = require('sortablejs')
 const storage = require('electron-json-storage');
 
-
 storage.setDataPath(os.homedir());
 
 const dataPath = storage.getDataPath();
@@ -24,8 +23,7 @@ var snackbar;
 document.addEventListener('DOMContentLoaded', function () {
     deleteModal = new bootstrap.Modal(document.getElementById('deleteModal'))
     addModal = new bootstrap.Modal(document.getElementById('addNewModal'))
-
-
+   
     var el = document.getElementById('sortable');
     var options = {
         group: 'share',
@@ -80,6 +78,8 @@ function getStorage() {
 
             var t1 = time.getTime()
 
+            x.id = x.id ?? Math.random().toString(36);
+
             const { otp, expires } = TOTP.generate(key, {
                 digits: x.info.digits,
                 period: x.info.period,
@@ -88,15 +88,22 @@ function getStorage() {
 
             var dif = t1 - expires;
             remainSeconds = Math.round(dif / 1000) * -1;
+            var optText = otp.toString().substring(0, 1) + " <span>" + otp.toString().substring(1, 4) + "</span> " + otp.toString().substring(4, 6);
+
 
             $(".twofa-list").append(`
                 <li class="row row-2fa" dataid="${x.id}">
                     <div class="col-9">
                         <label class="title">${x.name}</label>
-                        <label class="code">${otp}</label>
+                        <label class="code" otpdata="${otp}">${optText}</label>
                     </div>
                     <div class="col-1 time-wrapper">
-                        <div class="time">${remainSeconds}</div>
+                     <div id="countdown" name="countdown">
+                        <div class="time"><div></div></div>
+                            <svg>
+                                <circle r="18" cx="20" cy="20"></circle>
+                            </svg>
+                        </div>
                     </div>
                     <div class="col-1">
                         <a href="#" class="delete" dataid="${x.id}" data-bs-toggle="modal" data-bs-target=".delete-modal">Sil</a>
@@ -104,6 +111,9 @@ function getStorage() {
                 </div>
                 `)
         })
+        
+        $(document).trigger("onListLoaded");
+
     });
 }
 
@@ -120,81 +130,6 @@ function setStorage() {
     });
 }
 
-function writeData() {
-    var newdata = JSON.stringify(allData);
-    fs.writeFile(filePath,
-        newdata,
-        {
-            encoding: "utf8",
-            flag: "w",
-            mode: 0o666
-        }
-        , (err, newdata) => {
-
-            addModal.hide();
-            $("#addNewModal").find("input").val("");
-
-            deleteModal.hide();
-
-            if (err) {
-                console.error(err);
-                return;
-            }
-            else {
-                // readData()
-                getStorage();
-            }
-        })
-}
-
-function readData() {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        const response = JSON.parse(data)
-        // handle the json data here
-        $(".twofa-list").html("");
-
-        allData = response;
-
-        response.entries.sort((a, b) => a.queue - b.queue).map((x) => {
-
-            const key = x.info.secret
-
-            var time = new Date();
-
-            var t1 = time.getTime()
-
-            const { otp, expires } = TOTP.generate(key, {
-                digits: x.info.digits,
-                period: x.info.period,
-                timestamp: time.getTime(),
-            })
-
-            var dif = t1 - expires;
-            remainSeconds = Math.round(dif / 1000) * -1;
-
-            $(".twofa-list").append(`
-                <li class="row row-2fa" dataid="${x.id}">
-                    <div class="col-9">
-                        <label class="title">${x.name}</label>
-                        <label class="code">${otp}</label>
-                    </div>
-                    <div class="col-1 time-wrapper">
-                        <div class="time">${remainSeconds}</div>
-                    </div>
-                    <div class="col-1">
-                        <a href="#" class="delete" dataid="${x.id}" data-bs-toggle="modal" data-bs-target=".delete-modal">Sil</a>
-                    </div>
-                </div>
-                `)
-        })
-
-    });
-}
-
 function toast() {
     snackbar.className = "show";
     setTimeout(function () { snackbar.className = snackbar.className.replace("show", ""); }, 1500);
@@ -202,6 +137,22 @@ function toast() {
 
 $(document).ready(function () {
 
+    $(document).on("onListLoaded", function () {
+
+        $(".countdown").clone().appendTo(".time div");
+        
+        document.styleSheets[1].cssRules[30].style.animationDuration=remainSeconds+"s"
+
+        var countdownNumberEl = document.getElementsByClassName('.time div');
+        var countdown = remainSeconds;
+        
+        countdownNumberEl.textContent = countdown;
+        setInterval(function() {
+            countdown = --countdown <= 0 ? countdown-1 : countdown;
+            countdownNumberEl.textContent = countdown;
+        }, 1000);
+    });
+    
     snackbar = document.getElementById("toast");
 
     setInterval(function () {
@@ -212,14 +163,14 @@ $(document).ready(function () {
         }
         else {
             remainSeconds--;
-            $(".time").html(remainSeconds);
+            $(".time div").html(remainSeconds);
         }
 
     }, 1000)
 
     $(document).delegate(".row-2fa", "click", function (e) {
         var target = e.currentTarget;
-        var value = $(target).find(".code").html();
+        var value = $(target).find(".code").attr("otpdata");
         navigator.clipboard.writeText(value);
         toast();
     })
